f8ff02353dfafb6e34f89a6f6e4dddb1dd8f7ee9
Imran Hasan <imran072022@gmail.com>
2025-11-08 22:26:51 +0600

    an additional route (Games) created

--- src/Pages/Games.jsx
+++ src/Pages/Games.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GamesContext } from "../Contexts/GamesProvider";
+import Loading from "../Components/Loading";
+import GameCard from "../Components/GameCard";
 
 const Games = () => {
-  return <div>Explore all the games here</div>;
+  const { games, loading } = useContext(GamesContext);
+  console.log(games);
+  return (
+    <div className="max-w-7xl mx-auto my-20">
+      <h2 className="text-white text-5xl text-center  font-bold orbitron">
+        Explore Games
+      </h2>
+      <p className="text-white text-xl text-center mb-10 mt-2">
+        Discover and play your favorite games from all genres.
+      </p>
+      {loading ? (
+        <Loading></Loading>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
+          {games.map((game) => (
+            <GameCard key={game.id} game={game}></GameCard>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Games;
